19b3352d1b737183e2f409262f2529e2de7e4638
christopher pelech <christopherglynnpelech@gmail.com>
2016-05-09 11:10:58 -0500

    Cleaned up the graph

--- app/controllers/graph.js
+++ app/controllers/graph.js
@@ -4,32 +4,17 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 
   graphdata: Ember.computed("model",function(){
-			var r =Math.random(0,256).toString()
-			var g =Math.random(0,256)
-			var b =Math.random(0,256)
-			var rgba = "rgba(" + r + "," + g + "," + b + ",1)"
+			var r =Math.random(0,256).toString();
+			var g =Math.random(0,256).toString();
+			var b =Math.random(0,256).toString();
+			var rgbaz = "(" + r + "," + g + "," + b + ",1)"
 			return {
 				labels: this.get("model.listofdata").mapBy("date"),
 				datasets: [
 					{
 						label: "Stock Data",
-						fill: false,
-						lineTension: 0.1,
-						backgroundColor: rgba,
-						borderColor: "rgba(75,192,192,1)",
-						borderCapStyle: 'butt',
-						borderDash: [],
-						borderDashOffset: 0.0,
-						borderJoinStyle: 'miter',
-						pointBorderColor: "rgba(75,192,192,1)",
-						pointBackgroundColor: "#fff",
-						pointBorderWidth: 1,
-						pointHoverRadius: 5,
-						pointHoverBackgroundColor: "rgba(75,192,192,1)",
-						pointHoverBorderColor: rgba,
-						pointHoverBorderWidth: 2,
-						pointRadius: 1,
-						pointHitRadius: 10,
+						fill: true,
+   
 						data: this.get("model.listofdata").mapBy("average"),
 					}
 				]
